910bbc07c1e12020c2dc7b66d86ad6ce0f34a671
Stephen Davis <davi1138@purdue.edu>
2019-09-17 12:12:18 -0400

    created user model

---
+++ website/src/models/user.tsx
@@ -0,0 +1,8 @@
+export class User {
+    user_id:            number = -1;
+    user_first_name:    string = "";
+    user_last_name:     string = "";
+    user_email:         string = "";
+    user_password:      string = "";
+    user_tags:          []     = [];
+};

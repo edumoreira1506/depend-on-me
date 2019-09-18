e3e11b107b7714e668d247cd5f67abf22c02521b
Stephen Davis <davi1138@purdue.edu>
2019-09-18 17:49:10 -0400

    added documentation to models

--- website/src/models/http-requests.tsx
+++ website/src/models/http-requests.tsx
@@ -1,7 +1,29 @@
+/**
+ * data structure of body for the create-account POST http request
+ */
 export interface CreateAccountPOST {
+    /**
+     * username of the user. unique value used to identify the account in the database
+     */
     request_username:   string;
+
+    /**
+     * password for the account
+     */
     request_password:   string;
+
+    /**
+     * the email of the account. must be unique 
+     */
     request_email:      string;
+
+    /**
+     * the first name of the account
+     */
     request_first_name: string;
+
+    /**
+     * the last name of the account
+     */
     request_last_name:  string;
 }

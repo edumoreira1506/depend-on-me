0d3a9fe29c5fae57e3612aded59102858e6c14f8
nathancohen4299 <ncohen4299@gmail.com>
2019-09-26 15:22:23 -0400

    Created routes. Awaiting Database scripts

---
+++ depend_on_me_api/objs/User.py
@@ -0,0 +1,28 @@
+from flask_login.mixins import UserMixin
+
+class User(UserMixin):
+
+    # id: str
+    # password: str
+    # email :str
+    # first_name: str
+    # last_name: str
+    def __init__(self, id: str=None, password: str=None, email: str=None, first_name: str=None, last_name: str=None, privilege: int=None):
+        self.id = id
+        self.password = password
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.privilege = privilege
+        
+    def is_active(self):
+        return True
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
--- depend_on_me_api/routes/routes.py
+++ depend_on_me_api/routes/routes.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, url_for
+from flask_login import LoginManager, login_required
+from depend_on_me_api.objs import User
+from collections import namedtuple
+import json
 
-backend = Flask(__name__)
+backend = Flask(__name__, static_url_path='', static_folder='../../website/static/', template_folder="../../website/static/")
+login_manager = LoginManager()
 
 
 @backend.route("/")
-def hello():
+def root():
     return "200 OK"
 
 
+@backend.route("/create_account", methods=['POST'])
+def create_account(user: User):
+    potential_user = User(id=request.json["request_username"], password=request.json["request_password"], email=request.json["request_email"],
+             first_name=request.json["request_first_name"], last_name=request.json["request_last_name"])
+    #Do something with user
+    return "200 OK"
+
+
+@backend.route("/login", methods=['POST'])
+def login():
+    # Validate
+
+    #Redirect to another page
+    return redirect(url_for("index"))
+
+@backend.route("/index", methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User("", "", "", "", "", 0)
+
+
+
+
 if __name__ == "__main__":
+    login_manager.init_app(backend)
     backend.run(debug=True)

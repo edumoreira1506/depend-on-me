d6861e48a3b465ca12895e70696e8dd539b96b2c
Stephen Davis <davi1138@purdue.edu>
2019-09-30 10:50:18 -0400

    added support for custom types

--- website/src/components/accounts-page-container.tsx
+++ website/src/components/accounts-page-container.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import './component-styles.css';
-
-/**
- * simple wrapper for defining what type can de displayed by this component.
- */
-// NOTE: this is not redundant, moreso it is an infrastructure choice that will
-// allow for flexiblity within this component if and when design constraints 
-// change with the intent of reducing the overhead of refactoring code elsewhere
-type display_content = any;
-
+import { display_content } from '../models/types';
 
 /*
  * wrapper for all content displayed on the page 

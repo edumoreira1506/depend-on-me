5d7003ba509911602c23436dd71687589d68f05f
nathancohen4299 <ncohen4299@gmail.com>
2019-10-06 17:46:46 -0400

    Updated requirements.txt

--- tests/routes/test_routes.py
+++ tests/routes/test_routes.py
@@ -1,12 +1,13 @@
 import pytest
 from depend_on_me_api.routes.routes import backend
 
+
 @pytest.fixture
 def client():
     client = backend.test_client()
     yield client
 
+
 def test_hello_world(client):
     resp = client.get("/")
     assert resp.status == "200 OK"
-

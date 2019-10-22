0bc98b637905f777e3fe93b657caca40ae2e28a2
Stephen Davis <davi1138@purdue.edu>
2019-10-22 12:39:02 -0400

    [refactor] implemented page-container in landing-page

--- website/src/pages/landing-page.tsx
+++ website/src/pages/landing-page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 import H from 'history/index';
 import { PageService } from '../services/page-service';
-import { BlankSpace } from '../components/blank-space';
-
+import { PageContainer } from '../components/page-container';
 
 interface Props {
     history: H.History<any>;
@@ -16,37 +15,26 @@ export default class LandingPage extends React.Component<Props, State> {
 
     render() {
         return (
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="flex-start"
-                justify="center"
-                style={{minHeight: '100vh', padding: '10vw'}}
-            >
-                <Grid item xs={6} style={{minWidth: '20vw'}}>
-                    <Typography variant='h1'>depend on me
-                        {BlankSpace({height: '1vh'})}
-                        <Typography variant='h4'>description of depend on me</Typography>
-                    </Typography>
-                </Grid>
-                {BlankSpace({height:'5vh'})}
-                <Grid
-                    item xs={6}
-                    container
-                    spacing={4}
-                    direction="row"
-                    alignItems="center"
-                    justify="flex-start"
-                >
-                    <Grid xs={2} item>
-                        <Button style={{boxShadow: "none"}} color='primary' variant='contained' fullWidth onClick={() => PageService.redirect(this.props.history, '/login')}>login</Button>
-                    </Grid>
-                    <Grid md={2} item>
-                        <Button style={{boxShadow: "none", minWidth: '160px'}} color='primary' variant='contained' fullWidth onClick={() => PageService.redirect(this.props.history, '/createaccount')}>create account</Button>
-                    </Grid>
-                </Grid>
-            </Grid>
+            PageContainer ({
+                content: 
+                    <Grid
+                        item xs={6}
+                        container
+                        spacing={4}
+                        direction="row"
+                        alignItems="center"
+                        justify="flex-start"
+                    >
+                        <Grid xs={2} item>
+                            <Button style={{boxShadow: "none"}} color='primary' variant='contained' fullWidth onClick={() => PageService.redirect(this.props.history, '/login')}>login</Button>
+                        </Grid>
+                        <Grid md={2} item>
+                            <Button style={{boxShadow: "none", minWidth: '160px'}} color='primary' variant='contained' fullWidth onClick={() => PageService.redirect(this.props.history, '/createaccount')}>create account</Button>
+                        </Grid>
+                    </Grid>,
+                title: "depend on me",
+                subtitle: "desciption of depend on me"
+            })
         );
     }
 }

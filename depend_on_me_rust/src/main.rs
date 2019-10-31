b47e549d8753dc4ce09d06a279fc8fae021dd26b
Ryan Chen <ryanjchen2@gmail.com>
2019-10-31 12:32:02 -0400

    Added a get route for /account

--- depend_on_me_rust/src/main.rs
+++ depend_on_me_rust/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+ #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
 
@@ -7,10 +7,23 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/account/create")]
+#[post("/account")]
 fn create_user() -> &'static str {
-    "Hello world!"
+    /// Returns a JSON object with user information.
+    /// 
+    /// Takes in a the following arguments in a form:
+    ///  * request_username: str
+    ///  * request_first_name: str
+    ///  * request_last_name: str
+    ///  * request_password: str
+    ///  * request_email: str
+    ///
+    /// Returns:
+    ///  * 200 OK if successfule
+    ///  * 409 CONFLICT if username still exists
+    "Not implemented."
 }
+
 
 #[post("/login")]
 fn create_user() -> &'static str {
